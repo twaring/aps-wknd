da3ec99302ee6f9601c8d1329f77c77b894e45e0
Tim Waring <>
2023-12-08 15:21:21 +1100

    Update syntax.js

--- blocks/syntax/syntax.js
+++ blocks/syntax/syntax.js
@@ -1,4 +1,33 @@
 export default function decorate(block) {
     // do some cool logic in here
   }
-  
+
+  import { loadCSS, loadScript } from "../../scripts/aem.js";
+
+export default function decorate(block) {
+  // load the prism assets
+  loadScript("/blocks/syntax/prism.js");
+  loadCSS("/blocks/syntax/prism.css");
+}
+
+
+import { loadCSS, loadScript } from "../../scripts/aem.js";
+
+const decorateSyntaxLanguage = (block) => {
+  // add 2nd class to the code element if its not `block`
+  const syntaxLang = block.classList[1];
+  if (syntaxLang !== "block") {
+    block
+      .getElementsByTagName("code")[0]
+      .classList.add(`language-${syntaxLang}`);
+  }
+};
+
+export default function decorate(block) {
+  // load the prism assets
+  loadScript("/blocks/syntax/prism.js");
+  loadCSS("/blocks/syntax/prism.css");
+
+  // decorate each syntax block
+  decorateSyntaxLanguage(block);
+}
